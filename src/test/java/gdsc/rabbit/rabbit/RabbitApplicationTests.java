d93b814aaba54802629fa8811d467194002dad45
YehyeokBang <qkddpgur318@gmail.com>
2023-01-27 22:41:40 +0900

    chore: Edit Test file

--- src/test/java/gdsc/rabbit/rabbit/RabbitApplicationTests.java
+++ src/test/java/gdsc/rabbit/rabbit/RabbitApplicationTests.java
@@ -3,7 +3,6 @@ package gdsc.rabbit.rabbit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class RabbitApplicationTests {
 
     @Test
