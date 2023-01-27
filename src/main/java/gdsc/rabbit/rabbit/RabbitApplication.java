b4ec331bf81c8c99b37c6bcbc8041152a76d295c
YehyeokBang <qkddpgur318@gmail.com>
2023-01-27 21:13:13 +0900

    chore: Edit RabbitApplication.java

--- src/main/java/gdsc/rabbit/rabbit/RabbitApplication.java
+++ src/main/java/gdsc/rabbit/rabbit/RabbitApplication.java
@@ -2,8 +2,10 @@ package gdsc.rabbit.rabbit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class RabbitApplication {
 
     public static void main(String[] args) {
